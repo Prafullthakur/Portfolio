592563cb66e9b8c61b1ed159aa09b3cc8bf0230a
prafull <prafullchuahn45@gmail.com>
2020-04-13 18:58:10 +0530

    add

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 var vid = document.getElementById("myVideo");
 vid.playbackRate = 0.6;
+var navbar = document.querySelector(".nav-bar");
+var videoSection = document.querySelector(".video-section");
+var menu = document.querySelector(".Menu");
+var Part = document.querySelector(".part1");
+navbar.addEventListener('click',showMenu);
+var clickcount=0;
+function showMenu(e)
+{   console.log(e);
+    clickcount++;
+	if(clickcount%2==0)
+   { 
+     videoSection.style.display='inline-block';
+     menu.style.display='none';
+    }
+   else
+   {
+   	menu.style.display='inline-block';
+    videoSection.style.display='none';
+    
+   }
+}
