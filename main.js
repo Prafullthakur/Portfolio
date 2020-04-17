8f2fb576bbec5e95e3ed9efbb1f0034e84b318c3
prafull <prafullchuahn45@gmail.com>
2020-04-17 18:20:07 +0530

    add

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var vid = document.getElementById("myVideo");
 vid.playbackRate = 0.6;
 var navbar = document.querySelector(".nav-bar");
+var navline1 = document.querySelector(".nav-bar1");
+var navline2 = document.querySelector(".nav-bar2");
+var navline3 = document.querySelector(".nav-bar3");
 var videoSection = document.querySelector(".video-section");
 var menu = document.querySelector(".Menu");
 var Part = document.querySelector(".part1");
@@ -35,3 +38,18 @@ a1.forEach((items)=>{
 
 });
 });
+navbar.addEventListener('click',animate);
+function animate(e)
+{     if(clickcount%2==0)
+       {
+            navline1.className="nav-bar1";
+    navline2.className="nav-bar2";
+    navline3.className="nav-bar3";     
+       }
+    
+    else{
+    navline1.className+=" animateitonce";
+    navline2.className+=" animateittwice";
+    navline3.className+=" animateitthrice";
+     }
+}
